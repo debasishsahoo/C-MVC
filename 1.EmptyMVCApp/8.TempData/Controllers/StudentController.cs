c82930aeaefd4c934d7ebcf7ac5afba2c130b626
debasishsahoo <debasish.sahoo96@gmail.com>
2022-04-14 20:47:27 +0530

    Temp

---
+++ 1.EmptyMVCApp/8.TempData/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _8.TempData.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Method1()
+        {
+            TempData["Name"] = "MIR";
+            TempData["Age"] = 30;
+            return View();
+        }
+
+        public ActionResult Method2()
+        {
+            string Name;
+            int Age;
+
+            if(TempData.ContainsKey("Name"))
+                Name= TempData["Name"].ToString();
+
+            if (TempData.ContainsKey("Age"))
+                Age=int.Parse(TempData["Age"].ToString());
+
+            return View();
+        }
+
+        public ActionResult Method3()
+        {
+            string Name;
+            int Age;
+
+            if (TempData.ContainsKey("Name"))
+                Name = TempData["Name"].ToString();
+
+            if (TempData.ContainsKey("Age"))
+                Age = int.Parse(TempData["Age"].ToString());
+
+            return View();
+        }
+
+    }
+}
---
+++ 1.EmptyMVCApp/8.TempData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_8.TempData.MvcApplication" Language="C#" %>
