a04b01d780960fb92dfdfd97232554c451621f86
debasishsahoo <debasish.sahoo96@gmail.com>
2022-05-12 00:08:15 +0530

    full Handler

--- MesageHandler/MesageHandler/Models/MsgHandler1.cs
+++ MesageHandler/MesageHandler/Models/MsgHandler1.cs
@@ -9,6 +9,31 @@ using System.Threading.Tasks;
 
 namespace MesageHandler.Models
 {
+
+    // request                  response
+    //    |                        |
+    //    |                        |
+    //    |                        |
+    //    |                        |
+    //    |                        |
+    //    |  ____________________  |
+    //    |  Delegatiog Handler |  |
+    //    |  ____________________  |                   |
+    //    |                        |
+    //    |  ____________________  |
+    //    |    |INNER HANDLER |    |
+    //    |  ____________________  |
+
+    //SERVER-SIDE HTTP MESSAGE HANDLER
+
+    //httpServer               --> get the request from the HOST
+    //httpRoutingDispatcher    --> dispatches the request on the route
+    //httpControllerDisatcher  --> send the request to an Controller
+
+    //Read or Modify the HTTP request Header
+    //Add a response Header to the HTTP response
+    //Validate the request before they reach the Controller(Authentication & Authorization)
+
     public class MsgHandler1:DelegatingHandler
     {
         protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,CancellationToken cancellationToken)
